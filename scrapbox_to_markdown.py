b0dcdaa4abf35d8236dd95ddc398ba3babbd6432
minami <minaminaoy@gmail.com>
2021-12-13 17:23:54 +0900

    Fix

--- scrapbox_to_markdown.py
+++ scrapbox_to_markdown.py
@@ -65,24 +65,30 @@ for line_i, line in enumerate(lines):
 
     if line_i == 0:
         new_line = "# " + convert(line)
-    elif line.startswith(">"):
-        if line[1] != " ":
-            new_line = "> " + convert(line[1:])
+    else:
+        if code_block:
+            if indent[line_i] == 0:
+                new_lines.append("```\n")
+                code_block = False
+            else:
+                new_line = line[code_block_indent:]
+                new_lines.append(new_line)
+                continue
+
+        if line.startswith(">"):
+            if line[1] != " ":
+                new_line = "> " + convert(line[1:-1]) + "  \n"
+            else:
+                new_line = convert(line)
+        elif line.startswith("code:"):
+            code_block = True
+            code_block_indent = indent[line_i + 1]
+            if line == "code:txt\n":
+                new_line = "```\n"
+            else:
+                new_line = line.replace("code:", "```")
         else:
             new_line = convert(line)
-    elif line.startswith("code:"):
-        code_block = True
-        code_block_indent = indent[line_i + 1]
-        new_line = line.replace("code:", "```")
-    elif code_block:
-        if indent[line_i] == 0:
-            new_lines.append("```\n")
-            new_line = line
-            code_block = False
-        else:
-            new_line = line[code_block_indent:]
-    else:
-        new_line = convert(line)
 
     new_lines.append(new_line)
     
