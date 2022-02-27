b9b5bfbefaa51c50ad3ee0236bba3f6dc9dcb579
minami <minaminaoy@gmail.com>
2022-02-27 16:09:14 +0900

    孤立ページの結果をブラウザでまとめて開くスクリプトを追加

---
+++ open_links.py
@@ -0,0 +1,18 @@
+import webbrowser
+import urllib.parse
+
+links = open("result.txt").readlines()
+
+N = 10
+print(f"{len(links)} lines found.")
+
+for i in range(N):
+    if links[i].startswith("http"):
+        v = links[i].strip().split("//", 1)
+        v[-1] = urllib.parse.quote(v[-1])
+        url = "//".join(v)
+        print(url)
+        webbrowser.open(url)
+
+with open("result.txt", "w") as f:
+    f.writelines(links[N:])
