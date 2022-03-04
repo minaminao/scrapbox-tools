cf9297a919032935c8e1bfb8d4cceb428ec1eab2
minami <minaminaoy@gmail.com>
2022-03-04 20:15:01 +0900

    Nページ未満のときにエラーを吐くのを修正

--- open_links.py
+++ open_links.py
@@ -1,12 +1,12 @@
 import webbrowser
 import urllib.parse
 
-links = open("result.txt").readlines()
+links = open("result.txt").readlines()[:-1]
 
 N = 10
 print(f"{len(links)} lines found.")
 
-for i in range(N):
+for i in range(min(N, len(links))):
     if links[i].startswith("http"):
         v = links[i].strip().split("//", 1)
         v[-1] = urllib.parse.quote(v[-1])
